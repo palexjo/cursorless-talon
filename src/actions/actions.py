6c61237bd04c7826aca9ad01566db2e12f246ecf
Pokey Rule <pokey.rule@gmail.com>
2021-10-26 19:09:00 +0100

    Remove knausj conflict

--- src/actions/actions.py
+++ src/actions/actions.py
@@ -121,7 +121,7 @@ default_values = {
     "swap_action": {"swap": "swapTargets"},
     "move_bring_action": {"bring": "replaceWithTarget", "move": "moveToTarget"},
     "wrap_action": {"wrap": "wrapWithPairedDelimiter"},
-    "insert_snippet_action": {"snip": "insertSnippet"},
+    "insert_snippet_action": {"snippet": "insertSnippet"},
     "reformat_action": {"format": "applyFormatter"},
 }
 
